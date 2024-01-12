e1664409aa7814b820d7a9305c88fe84e5a7522b
[treasure3210] <treasure3210@gmail.com>
2024-01-11 19:09:03 -0700

    Initial commit

---
+++ landmark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  landmark
+//
+//  Created by Derek Kim on 2024-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
