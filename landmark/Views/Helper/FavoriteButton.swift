66f5b59b051d7258d4eacf67b227a67ef0606bee
[treasure3210] <treasure3210@gmail.com>
2024-01-13 20:17:07 -0700

    Create reusable FavoriteButon View, Restructure folders

---
+++ landmark/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  landmark
+//
+//  Created by Derek Kim on 2024-01-13.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
